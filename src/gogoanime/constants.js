b2d990510772e30451a6b150dd85f3fe6bd7c1e4
pwn0x80 <adityashrest.shrest34@gmail.com>
2021-12-29 06:49:35 +0530

    Update constants.js

--- src/gogoanime/constants.js
+++ src/gogoanime/constants.js
@@ -1,5 +1,5 @@
 module.exports = {
 	NAME: 'GOGOANIME',
-	URL_SEARCH: 'https://www5.gogoanime.pro/search?keyword=',
-	URL_BASE: 'https://www5.gogoanime.pro'
+	URL_SEARCH: 'https://ww1.gogoanime2.org/search?keyword=',
+	URL_BASE: 'https://ww1.gogoanime2.org'
 };
